d91a63022b5ae8a4577cd18d04902ea669f662eb
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2024-03-24 19:58:32 +0530

    Create DFS.java

---
+++ Graph/DFS.java
@@ -0,0 +1,21 @@
+void dfs(int node,ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> res){
+  vis[node]=true;
+  res.add(node);
+
+  for(Iterator itr : adj.get(node)){
+    if(!vis[itr])
+      dfs(itr,adj,res);
+  }
+  
+}
+
+ArrayList<Integer> DFSTraversal(int V, ArrayList<ArrayList<Integer>> adj){
+  boolean[] vis = new boolean[V];
+  ArrayList<Integer> res = new ArrayList<>();
+  for(int i=0;i<V;i++){
+    if(!vis[i]){
+      dfs(i,adj,res);
+    }
+  }
+  return res;
+}
