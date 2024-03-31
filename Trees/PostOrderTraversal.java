f81a1bf6cc56030aee99455b17b7d797cff135ec
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2024-03-31 11:55:02 +0530

    Update PostOrderTraversal.java

--- Trees/PostOrderTraversal.java
+++ Trees/PostOrderTraversal.java
@@ -1,4 +1,14 @@
-
+Class TreeNode{
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int val,TreeNode left, TreeNode right){
+    this.val=val;
+    this.left=left;
+    this.right=right;
+  }
+};
+  
 // recursion
 void postorder(TreeNode root){
   if(root==null)
@@ -7,3 +17,25 @@ void postorder(TreeNode root){
   postorder(root.right);
   res.add(root.val);
 }
+
+
+//Using stack
+ public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res= new ArrayList<Integer>();
+        Stack<TreeNode> stk= new Stack<>();
+        if(root==null)
+            return res;
+        stk.add(root);
+        while(!stk.isEmpty()){
+            root=stk.peek();
+            stk.pop();
+            res.add(root.val);
+             if(root.left!=null)
+                stk.push(root.left);
+             if(root.right!=null)
+                stk.push(root.right);
+            
+        }
+        Collections.reverse(res);
+        return res;
+    }
