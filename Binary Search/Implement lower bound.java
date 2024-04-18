fb681f5df48ac8c7c2568b58d6ba4e6f70fabbb5
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2024-04-18 19:25:01 +0530

    Create Implement lower bound.java

---
+++ Binary Search/Implement lower bound.java
@@ -0,0 +1,21 @@
+     // Write your code here
+        int start =0;
+        int end = n-1;
+        int mid;
+        int ans =-1;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(arr[mid]>=x){
+                  ans =  mid;
+                  end = mid-1;
+            }
+              
+            else if(arr[mid]<x)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+       if(ans==-1)
+        return n;
+        return ans;
+    }
