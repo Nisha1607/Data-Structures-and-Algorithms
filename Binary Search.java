41eaa06889902462d7216eec254813fb20c9413a
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2024-04-18 13:42:41 +0530

    Create Binary Search.java

---
+++ Binary Search.java
@@ -0,0 +1,19 @@
+  public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        int mid;
+
+        while(start<=end){
+            mid = (start+end)/2;
+            if(target == nums[mid])
+                return mid;
+            else if(target > nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+
+        return -1;
+    }
