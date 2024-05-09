cdffac3e761157c91477a64af7bfae84a686d408
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2024-05-09 23:52:14 +0530

    Create Closest Dessert cost.java

---
+++ Backtracking/Closest Dessert cost.java
@@ -0,0 +1,26 @@
+class Solution {
+    int min = Integer.MAX_VALUE;
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        int[] result=new int[1];
+        
+        for(int base : baseCosts){
+            dfs(toppingCosts,0,base,target,result);
+        }
+
+        return result[0];
+    }
+
+    void dfs(int[] toppingCosts,int i,int cost,int target,int[] res){
+        if(i==toppingCosts.length){
+            if(Math.abs(target-cost)<min || (Math.abs(target-cost)==min && cost < res[0])){
+                res[0]=cost;
+                min = Math.abs(target-cost);
+            }
+            return;
+        }
+
+        dfs(toppingCosts,i+1,cost,target,res);
+        dfs(toppingCosts,i+1,cost+toppingCosts[i],target,res);
+        dfs(toppingCosts,i+1,cost+toppingCosts[i]*2,target,res);
+    }
+}
