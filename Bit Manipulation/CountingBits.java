c570266aec43fa92b8188184d720bf565b3f884a
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2025-01-28 18:58:46 +0530

    Create CountingBits.java

---
+++ Bit Manipulation/CountingBits.java
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/counting-bits/description/
+
+
+class Solution {
+    public int[] countBits(int n) {
+       int[] ans = new int[n+1];
+       for(int i=1;i<=n;i++){
+        ans[i]=ans[i>>1] + (i&1);
+       }
+       return ans;
+    }
+}
