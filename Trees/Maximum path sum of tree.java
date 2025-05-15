a58db620518ca8e8130596ac081a4763d8442b68
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2025-05-15 07:56:08 +0530

    Update Maximum path sum of tree.java

--- Trees/Maximum path sum of tree.java
+++ Trees/Maximum path sum of tree.java
@@ -3,6 +3,6 @@ int maxPathSum(TreeNode root,int sum){
   if(root==null) return 0;
   int left = maxPathSum(root.left);
   int right = maxPathSum(root.right);
-  sum = Math.max(sum, root.val + left + val + root.val);
+  sum = Math.max(sum, root.val + left + right);
   return root.val + Math.max(left,right);
 }
